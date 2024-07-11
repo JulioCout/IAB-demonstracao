9b2f96b90cd615889dd339b27d78759385fe2ef3
juliosilvacoutinho <juliosilvacoutinho@gmail.com>
2024-07-11 01:54:26 +0000

    personalizar para iab

--- src/components/Dashboard/Views/Pages/Layout/AppFooter.vue
+++ src/components/Dashboard/Views/Pages/Layout/AppFooter.vue
@@ -2,23 +2,10 @@
   <footer class="footer footer-black footer-white">
     <div class="container-fluid">
       <div class="row">
-        <nav class="footer-nav">
-          <ul>
-            <li>
-              <a href="https://www.creative-tim.com" target="_blank" rel="noopener">Creative Tim</a>
-            </li>
-            <li>
-              <a href="http://blog.creative-tim.com/" target="_blank" rel="noopener">Blog</a>
-            </li>
-            <li>
-              <a href="https://www.creative-tim.com/license" target="_blank" rel="noopener">Licenses</a>
-            </li>
-          </ul>
-        </nav>
         <div class="credits ml-auto">
           <div class="copyright">
             &copy; Coded with
-            <i class="fa fa-heart heart"></i> by <a href="https://www.creative-tim.com/?ref=pdf-vuejs" target="_blank" rel="noopener">Creative Tim</a>.
+            <i class="fa fa-heart heart"></i> by <a href="" target="_blank" rel="noopener">Julio Coutinho</a>.
           </div>
         </div>
       </div>
